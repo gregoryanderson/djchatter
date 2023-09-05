616aab9b727437ea17274277987529a18474384e
Gregory Anderson <30326085+gregoryanderson@users.noreply.github.com>
2023-09-05 15:40:59 -0600

    add schema and docs for schema

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -28,6 +28,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #external
+    "drf_spectacular",
+    #internal
     "account",
     "server",
 ]
@@ -116,3 +119,14 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': True,
+}

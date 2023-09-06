8092db4f92fa273ee98d455129054b9051feda4c
Gregory Anderson <30326085+gregoryanderson@users.noreply.github.com>
2023-09-06 11:21:04 -0600

    Add number of members to server call

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Count
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
@@ -14,6 +15,7 @@ class ServerListViewSet(viewsets.ViewSet):
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
         by_serverid = request.query_params.get("by_serverid")
+        with_num_members = request.query_params.get("with_num_members") == "true"
 
         if by_user or by_serverid and not request.user.is_authenticated:
             raise AuthenticationFailed()
@@ -25,18 +27,24 @@ class ServerListViewSet(viewsets.ViewSet):
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
 
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
+
         if qty:
             self.queryset = self.queryset[: int(qty)]
-        
+
         if by_serverid:
             try:
                 self.queryset = self.queryset.filter(id=by_serverid)
                 if not self.queryset.exists():
-                    raise ValidationError(detail=f"Server with id {by_serverid} no found")
-                
+                    raise ValidationError(
+                        detail=f"Server with id {by_serverid} no found"
+                    )
+
             except ValueError:
                 raise ValidationError(detail=f"Server with id {by_serverid} no found")
 
-
-        serializer = ServerSerializer(self.queryset, many=True)
+        serializer = ServerSerializer(
+            self.queryset, many=True, context={"num_members": with_num_members}
+        )
         return Response(serializer.data)

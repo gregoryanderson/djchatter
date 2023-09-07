2de65cac383d06156fc470d9d8d9eef664a25b1a
Gregory Anderson <30326085+gregoryanderson@users.noreply.github.com>
2023-09-07 13:22:51 -0600

    Add material ui

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,25 @@
-const Home = () => {
-    return (
-        <>
-            <p>Home</p>
-        </>
-    )
-}
+import { Box, CssBaseline } from "@mui/material";
 
-export default Home
+const Home = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          width: 300,
+          height: 300,
+          backgroundColor: "primary.dark",
+          "&:hover": {
+            backgroundColor: "primary.main",
+            opacity: [0.9, 0.8, 0.7],
+          },
+          display: "flex",
+        }}
+      >
+        <CssBaseline />
+        Home
+      </Box>
+    </>
+  );
+};
+
+export default Home;

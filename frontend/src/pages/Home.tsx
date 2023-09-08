4f9c5206fe2ac75b91f1c3727166a1d3a3fa5179
Gregory Anderson <30326085+gregoryanderson@users.noreply.github.com>
2023-09-08 13:50:51 -0600

    Begin the frontend work

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,21 +1,16 @@
 import { Box, CssBaseline } from "@mui/material";
+import PrimaryAppBar from "./templates/PrimaryAppBar";
 
 const Home = () => {
   return (
     <>
       <Box
         sx={{
-          width: 300,
-          height: 300,
-          backgroundColor: "primary.dark",
-          "&:hover": {
-            backgroundColor: "primary.main",
-            opacity: [0.9, 0.8, 0.7],
-          },
           display: "flex",
         }}
       >
         <CssBaseline />
+        <PrimaryAppBar />
         Home
       </Box>
     </>
